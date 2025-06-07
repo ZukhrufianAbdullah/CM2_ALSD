a2c26d31d7b75094cb444dedafa884af3df894c4
ZukhrufianAbdullah <zukrufianabdullah@gmail.com>
2025-06-07 22:52:20 +0700

    fix merge

--- AntrianKendaraan.java
+++ AntrianKendaraan.java
@@ -1,3 +1,70 @@
 public class AntrianKendaraan {
-    
+
+    Node head;
+    int jumlah = 0;
+
+        public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void tambahAntrian() {
+        Kendaraan k = new Kendaraan();
+        k.inputData();
+        Node baru = new Node(k);
+        if (isEmpty()) {
+            head = baru;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = baru;
+        }
+        jumlah++;
+        System.out.println(">> Kendaraan masuk ke dalam antrian.");
+    }
+
+    public Kendaraan layani() {
+        if (isEmpty()) return null;
+        Kendaraan dilayani = head.data;
+        head = head.next;
+        jumlah--;
+        return dilayani;
+    }
+
+    public void tampilAntrian() {
+        if (isEmpty()) {
+            System.out.println(">> Tidak ada kendaraan dalam antrian.");
+            return;
+        }
+        System.out.println("Antrian Kendaraan:");
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            System.out.println("----------------------");
+            temp = temp.next;
+        }
+    }
+
+    public int hitungAntrian() {
+        return jumlah;
+    }
+
+    public void layaniDenganTransaksi(QueueTransaksi transaksiQueue, BBM[] daftarBBM) {
+        Kendaraan k = layani();
+        if (k == null) {
+            System.out.println(">> Tidak ada kendaraan dalam antrian.");
+            return;
+        }
+        System.out.println("Petugas melayani " + k.platNomor);
+        Transaksi t = new Transaksi();
+        t.inputTransaksi(k.platNomor);
+        transaksiQueue.enqueue(t);
+        System.out.println(">> Transaksi berhasil dicatat.");
+    }
+
+    public Kendaraan peek() {
+        if (isEmpty()) return null;
+        return head.data;
+    }
 }
--- Kendaraan.java
+++ Kendaraan.java
@@ -1,3 +1,21 @@
-public class Kendaraan{
-    
+import java.util.Scanner;
+
+public class Kendaraan {
+    String platNomor;
+    String jenisKendaraan;
+    String merk;
+
+    public void inputData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Plat Nomor: ");
+        platNomor = sc.nextLine();
+        System.out.print("Masukkan Jenis Kendaraan: ");
+        jenisKendaraan = sc.nextLine();
+        System.out.print("Masukkan Merk: ");
+        merk = sc.nextLine();
+    }
+
+    public String toString() {
+        return "Plat Nomor: " + platNomor + "\nTipe: " + jenisKendaraan + "\nMerk: " + merk;
+    }
 }
